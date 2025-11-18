a92a03e2877ac2fff5979bbe142fb244a315bc1a
Yuhan-Fan <yh.fan@mail.utoronto.ca>
2025-11-17 21:55:51 -0500

    Implement StartFragment (first version)

---
+++ app/src/main/java/com/example/smartair/StartFragment.java
@@ -0,0 +1,41 @@
+package com.example.smartair;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+
+public class StartFragment extends Fragment {
+
+    Button btnLogin;
+    Button btnRegister;
+
+    public StartFragment() {
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_start, container, false);
+
+        btnLogin = view.findViewById(R.id.button_login);
+        btnRegister = view.findViewById(R.id.button_register);
+
+        btnLogin.setOnClickListener(v -> {
+            NavController navController = Navigation.findNavController(requireView());
+            navController.navigate(R.id.loginFragment);
+        });
+
+        btnRegister.setOnClickListener(v -> {
+            NavController navController = Navigation.findNavController(requireView());
+            navController.navigate(R.id.action_startFragment_to_roleSelectionFragment);
+        });
+        return view;
+    }
+}
