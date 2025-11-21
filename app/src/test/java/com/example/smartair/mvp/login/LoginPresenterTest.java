6dd207036f341541fcbf62179542c1e9df28768c
Yuhan-Fan <yh.fan@mail.utoronto.ca>
2025-11-21 18:49:41 -0500

    fix: add import for LoginPresenterTest

--- app/src/test/java/com/example/smartair/mvp/login/LoginPresenterTest.java
+++ app/src/test/java/com/example/smartair/mvp/login/LoginPresenterTest.java
@@ -1,6 +1,11 @@
 package com.example.smartair.mvp.login;
 
+import com.example.smartair.ui.mvp.login.LoginModel;
+import com.example.smartair.ui.mvp.login.LoginPresenter;
+import com.example.smartair.ui.mvp.login.LoginView;
+
 import org.junit.runner.RunWith;
+import org.mockito.Mock;
 import org.mockito.junit.MockitoJUnitRunner;
 
 @RunWith(MockitoJUnitRunner.class)
