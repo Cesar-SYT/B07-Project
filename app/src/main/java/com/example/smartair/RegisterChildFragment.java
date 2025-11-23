3d419f5e9cc50e5b9f1e7a48f853f4d5ad227876
Yuhan-Fan <yh.fan@mail.utoronto.ca>
2025-11-23 11:05:24 -0500

    fix: update stashed files

---
+++ app/src/main/java/com/example/smartair/RegisterChildFragment.java
@@ -0,0 +1,24 @@
+package com.example.smartair;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+public class RegisterChildFragment extends Fragment {
+
+
+    public RegisterChildFragment() {
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_register_child, container, false);
+    }
+}
