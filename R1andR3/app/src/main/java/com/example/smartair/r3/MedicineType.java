88000d3d0c581878bf689aeedd3d0e2d60b6c171
Cesar <cesar_syt@songyitaodeair.home>
2025-11-29 21:07:46 -0500

    chore: clear branch

--- R1andR3/app/src/main/java/com/example/smartair/r3/MedicineType.java
+++
@@ -1,6 +0,0 @@
-package com.example.smartair.r3;
-
-public enum MedicineType {
-    RESCUE,
-    CONTROLLER
-}
--- R1andR3/app/src/main/java/com/example/smartair/r3/StreakType.java
+++
@@ -1,6 +0,0 @@
-package com.example.smartair.r3;
-
-public enum StreakType {
-    CONTROLLER_DAYS,          // Continue taking the controller as scheduled.
-    TECHNIQUE_COMPLETED_DAYS  // Continuous Completion Skills Session
-}
