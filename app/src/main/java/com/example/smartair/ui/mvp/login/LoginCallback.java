2f8d3f5c4912beb6507c6999d51f00335241f354
Yuhan-Fan <yh.fan@mail.utoronto.ca>
2025-12-01 00:40:40 -0500

    Revert "Merge remote-tracking branch 'origin/feat/douglas/loginsignup' into combine-r1r3r5"
    This reverts commit e243cd15f37b643250cf94335d99839eac8fde9f, reversing
    changes made to 5abf38cf8b4c27880815bca01aca307ab0f09144.

--- app/src/main/java/com/example/smartair/ui/mvp/login/LoginCallback.java
+++
@@ -1,6 +0,0 @@
-package com.example.smartair.ui.mvp.login;
-
-public interface LoginCallback {
-    void onSuccess();
-    void onFailure(String errorMessage);
-}
--- app/src/main/java/com/example/smartair/ui/mvp/login/LoginModel.java
+++
@@ -1,5 +0,0 @@
-package com.example.smartair.ui.mvp.login;
-
-public interface LoginModel {
-    void login(String email, String password, String callback);
-}
